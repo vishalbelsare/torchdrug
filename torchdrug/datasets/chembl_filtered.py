b779eefbd3c17990690a5860920c08f00f8fb10a
Oxer11 <17300240035@fudan.edu.cn>
2021-09-26 10:59:59 -0400

    fix bugs in loading chembl filtered

--- torchdrug/datasets/chembl_filtered.py
+++ torchdrug/datasets/chembl_filtered.py
@@ -19,7 +19,7 @@ from torchdrug.utils import doc
 class ChEMBLFiltered(data.MoleculeDataset):
     """
     Statistics:
-        - #Molecule: 430,711
+        - #Molecule: 430,710
         - #Regression task: 1,310
 
     Parameters:
@@ -28,23 +28,20 @@ class ChEMBLFiltered(data.MoleculeDataset):
         **kwargs
     """
 
-    url = "http://snap.stanford.edu/gnn-pretrain/data/chem_dataset.zip"
-    md5 = "e95da4dffa0fdb1d4af2726bdf8c23e0"
+    url = "https://zenodo.org/record/5528681/files/chembl_filtered_torchdrug.csv.gz"
+    md5 = "2fff04fecd6e697f28ebb127e8a37561"
 
     def __init__(self, path, verbose=1, **kwargs):
         path = os.path.expanduser(path)
         if not os.path.exists(path):
             os.makedirs(path)
         self.path = path
-        zip_file_name = utils.download(self.url, path, md5=self.md5)
-        utils.extract(zip_file_name)
-
-        file_name = preprocess('./temp/chem_dataset')
-        print('file_name is {}'.format(file_name))
+        zip_file = utils.download(self.url, path, md5=self.md5)
+        csv_file = utils.extract(zip_file)
 
         self.target_fields = ["target_{}".format(i) for i in range(1310)]
 
-        self.load_csv(file_name, smiles_field="smiles", target_fields=self.target_fields,
+        self.load_csv(csv_file, smiles_field="smiles", target_fields=self.target_fields,
                       verbose=verbose, **kwargs)
         return
 
